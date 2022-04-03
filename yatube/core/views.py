0d16b888f29b9437c593748ee7270881d7753c69
Виктор Постников <G.abrie1@yandex.ru>
2022-04-04 01:43:39 +0500

    1

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
+
 def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
+
 
 def page_500(request, exception):
     return render(request, 'core/500.html', {'path': request.path}, status=500)
