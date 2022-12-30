d357a5a9083a8b12cda1512c8d1c5832c43432fc
Виктор Постников <G.abrie1@yandex.ru>
2022-12-30 16:54:22 +0500

    'static'

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'core.apps.CoreConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +57,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
@@ -18,6 +17,5 @@ handler403 = 'core.views.csrf_failure'
 handler500 = 'core.views.page_500'
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
